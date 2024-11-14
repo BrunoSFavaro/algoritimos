c88654e48ecdf2e390719d332010535f35ec9a4c
BrunoSFavaro <bruno-silvafavaro@hotmail.com>
2024-11-14 10:53:01 -0300

    Funções criadas
    Algorítmo funcionando com códigos sem "?"

--- C&IT/morse.py
+++ C&IT/morse.py
@@ -1,3 +1,4 @@
+#Mapeamento das letras a serem usadas no desafio
 dicionario = {
     ".": "E",
     "..": "I",
@@ -14,3 +15,20 @@ dicionario = {
     "--.": "G",
     "---": "O"
 }
+
+def combinations(morse_code):
+    if "?" not in morse_code:
+        return [morse_code]     #Retorna o código original se não houver "?"
+    
+    return combinations
+
+
+
+def morse_to_text(morse_code):
+    possibilities = combinations(morse_code) #Usa a outra função para encontrar as possíveis combinações
+    results = [dicionario.get(code) for code in possibilities if code in dicionario] #Filtra as combinações que existem no dicionário
+    return results
+
+codigo = input()
+resultado = morse_to_text(codigo)
+print(resultado)
