e950b08aea3d25808f2e37fbdb030dc1d9dd49e1
BrunoSFavaro <bruno-silvafavaro@hotmail.com>
2024-11-14 11:04:57 -0300

    Função "combinations" pronta

--- C&IT/morse.py
+++ C&IT/morse.py
@@ -19,6 +19,26 @@ dicionario = {
 def combinations(morse_code):
     if "?" not in morse_code:
         return [morse_code]     #Retorna o código original se não houver "?"
+         #Lista para armazenar as combinações possíveis
+    combinations = []
+    for char in morse_code:
+        #Se o caractere é "?", cria combinações com "." e "-"
+        if char == "?":
+            if not combinations:            #Combionations vazio
+                combinations = [".", "-"]   #Inicializa com as duas possibilidades para '?'
+            else:
+                combinations = [combo + "." for combo in combinations] +  [combo + "-" for combo in combinations] #Expande cada combinação já existente com '.' e '-'
+        #Se o caractere não for '?'
+        else:
+            #E combinations estiver vazio
+            if not combinations:
+                #Inicializa com o caractere
+                combinations = [char]
+            #Se combinations não estiver vazio
+            else:
+                #Adiciona o caractere a cada combinação já existente
+                combinations = [combo + char for combo in combinations]
+
     
     return combinations
 
