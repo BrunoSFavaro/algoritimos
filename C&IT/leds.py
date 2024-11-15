b9d632527c8573f06e13e82a08b46272ba5af487
BrunoSFavaro <bruno-silvafavaro@hotmail.com>
2024-11-15 12:33:03 -0300

    Código comentado

--- C&IT/leds.py
+++ C&IT/leds.py
@@ -1,10 +1,8 @@
 def calcula_total_leds(altura,largura):
-  if(altura <= 0 or largura <= 0):
-    leds = 0
-  else:
-    altura += 1
-    largura += 1
-    leds = altura * largura
-  return leds
+  if(altura <= 0 or largura <= 0): #Tratando o caso de 0 de números negativos como entrada
+    return 0
+  else: 
+    leds = (altura+1) * (largura+1) #Calcula os leds (Os vértices dos quadrados no retângulo)
+    return leds
 
 print(calcula_total_leds(3,4))
